8b0ec98917a263af076b235c85768134948aafc7
YuanSyun <rty3497374@gmail.com>
2019-05-30 14:31:49 +0800

    finish task1

--- myKNN.m
+++ myKNN.m
@@ -12,18 +12,34 @@ else
     testSetLength = length(testLabel);
     trainSetLength = length(trainLabel);
     predictLabel = zeros(testSetLength, 1);
+    labelNum = trainLabel(end);
+    
     for i = 1:testSetLength
         
+        % calcuate the European distance with label
         distances = zeros(trainSetLength, 2);
         for j = 1:trainSetLength
-           distances(j, 1) = dist(testSet(i), trainSet(j)');
+           sample = testSet(i,:);
+           train = trainSet(j,:);
+           distances(j, 1) = dist(train, sample');
            distances(j, 2) = trainLabel(j);
         end
 
-        distances = sort(distances, 1);
-        kDistance = distances(1:kNumber,1);
-        [maxTimes, index] = max(kDistance);
-        predictLabel(i) = distances(index, 2);     
+        % sort the distance and pick up the k number smaple
+        distances = sortrows(distances, 1);
+        kDistance = distances(1:kNumber,:);
+        
+        % vote the label
+        voteLabels = zeros(labelNum, 1);
+        for k = 1:kNumber
+            index = uint8(kDistance(k,2));
+            voteLabels(index) = voteLabels(index) + 1;
+        end
+        
+        % pick up the most voted label
+        [maxTimes, index] = max(voteLabels);
+        predictLabel(i) = index;   
+        
     end
     accuracy = getAccuracy(predictLabel, testLabel);
 end
